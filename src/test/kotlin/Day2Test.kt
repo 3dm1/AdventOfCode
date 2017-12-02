3921c99b19815ec6e4db603a198b153b9b890006
Edson Menegatti <edson.menegatti@gmail.com>
2017-12-02 19:57:34 +0100

    Add solution to Day 2

---
+++ src/test/kotlin/Day2Test.kt
@@ -0,0 +1,25 @@
+import day2.checksumDivision
+import day2.checksumMinMax
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day2Test {
+
+    @Test
+    fun part1() {
+        val input = """
+            |5 1 9 5
+            |7 5 3
+            |2 4 6 8""".trimMargin()
+        assertEquals(checksumMinMax(input), 18)
+    }
+
+    @Test
+    fun part2() {
+        val input = """
+        |5 9 2 8
+        |9 4 7 3
+        |3 8 6 5""".trimMargin()
+        assertEquals(checksumDivision(input), 9)
+    }
+}
