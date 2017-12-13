aa870f03b9c0dc2ec421e006a101fa3dda6f994c
Edson Menegatti <edson.menegatti@gmail.com>
2017-12-13 15:54:34 +0100

    Add alternative solution to Day 13

--- src/main/kotlin/day13/PacketScanners.kt
+++ src/main/kotlin/day13/PacketScanners.kt
@@ -6,11 +6,15 @@ fun findMatches(input: Map<Int, Int>, delay: Int = 0) = input.entries.filter { (
 
 fun getSeverity(input: Map<Int, Int>, delay: Int = 0) = findMatches(input, delay).sumBy { it.key * it.value }
 
-fun getMinDelay(input: Map<Int, Int>) = (0..10000000).asSequence().first { findMatches(input, it).isEmpty() }
+fun getMinDelay(input: Map<Int, Int>) = generateSequence(0, Int::inc).first { findMatches(input, it).isEmpty() }
+
+fun getMinDelayFaster(input: Map<Int, Int>) = generateSequence(0, Int::inc)
+        .first { delay -> input.entries.none { (delay + it.key) % (2 * (it.value - 1)) == 0 } }
 
 fun main(args: Array<String>) {
     val rawInput = resourceToList("day13_packet_scanners.txt")
     val input = rawInput.map { it.split(": ") }.associate { it[0].toInt() to it[1].toInt() }
     println(getSeverity(input))
     println(getMinDelay(input))
+    println(getMinDelayFaster(input))
 }
