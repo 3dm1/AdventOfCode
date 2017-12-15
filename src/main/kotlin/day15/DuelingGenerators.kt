7eeea67ab46f1a67e3f5d5fff3e2a5dabd99fc8d
Edson Menegatti <edson.menegatti@gmail.com>
2017-12-15 10:03:27 +0100

    Update DuelingGenerators with generateSequence due to better performance
    buildSequence took on average 700ms more to execute part 2

--- src/main/kotlin/day15/DuelingGenerators.kt
+++ src/main/kotlin/day15/DuelingGenerators.kt
@@ -1,20 +1,14 @@
 package day15
 
-import kotlin.coroutines.experimental.buildSequence
-
 const val GEN_A_SEED = 634L
 const val GEN_B_SEED = 301L
 
-fun createGenerator(seed: Long, multiplyingFactor: Int, pickyFactor: Int = 1) = buildSequence {
-    var value = seed
-    while (true) {
-        value = (value * multiplyingFactor) % 2147483647
-        if (value % pickyFactor == 0L) yield(value)
-    }
+fun createGenerator(seed: Long, multiplyingFactor: Int, pickyFactor: Int = 1): Sequence<Long> {
+    return generateSequence(seed) { (it * multiplyingFactor) % 2147483647 }.drop(1).filter { it % pickyFactor == 0L }
 }
 
 fun getMatchesCount(genA: Sequence<Long>, genB: Sequence<Long>, iterationCount: Int): Int {
-    return genA.take(iterationCount).zip(genB).count { (it.first and 0xFFFF) == (it.second and 0xFFFF) }
+    return genA.zip(genB).take(iterationCount).count { (it.first and 0xFFFF) == (it.second and 0xFFFF) }
 }
 
 fun main(args: Array<String>) {
