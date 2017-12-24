710053c3c7658133da0113e3b9d50fa25d65224e
Edson Menegatti <edson.menegatti@gmail.com>
2017-12-24 13:01:31 +0100

    Add solution to Day 24

---
+++ src/main/kotlin/day24/ElectromagneticMoat.kt
@@ -0,0 +1,47 @@
+package day24
+
+import common.resourceToList
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+
+fun parseInput(input: List<String>): Map<Int, List<Pair<Int, Int>>> {
+    return input.map { conn -> conn.split("/").map(String::toInt) }
+            .flatMap { listOf(it[0] to (it[0] to it[1]), it[1] to (it[0] to it[1])) }
+            .groupBy { it.first }
+            .map { it.key to it.value.map { it.second }.distinct() }
+            .toMap()
+}
+
+fun getPossibleConnections(connMap: Map<Int, List<Pair<Int, Int>>>,
+                           currentPin: Int = 0,
+                           currentConnection: Set<Pair<Int, Int>> = emptySet(),
+                           connections: MutableSet<Set<Pair<Int, Int>>>) {
+    connections.add(currentConnection)
+    connMap[currentPin]
+            ?.filter { it !in currentConnection && currentConnection + it !in connections }
+            ?.forEach {
+                val newConn = currentConnection + it
+                val newPin = if (it.first != currentPin) it.first else it.second
+                getPossibleConnections(connMap, newPin, newConn, connections)
+            }
+}
+
+fun getConnectionValue(connection: Set<Pair<Int, Int>>): Int {
+    return connection.map { it.first + it.second }.sum()
+}
+
+fun getBestBridge(bridges: MutableSet<Set<Pair<Int, Int>>>): Int? {
+    val maxSize = bridges.maxBy { it.size }?.size
+    return bridges.filter { it.size == maxSize }.map(::getConnectionValue).max()
+}
+
+fun main(args: Array<String>) {
+    val rawInput = resourceToList("day24_electromagnetic_moat.txt")
+    val connMap = parseInput(rawInput)
+
+    val connections = mutableSetOf<Set<Pair<Int, Int>>>()
+    getPossibleConnections(connMap, connections = connections)
+    println(connections.size)
+    println(connections.map(::getConnectionValue).max())
+    println(getBestBridge(connections))
+}
---
+++ src/test/kotlin/Day24Test.kt
@@ -0,0 +1,61 @@
+import day24.getBestBridge
+import day24.getConnectionValue
+import day24.getPossibleConnections
+import day24.parseInput
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day24Test {
+
+    private val rawInput = """
+    |0/2
+    |2/2
+    |2/3
+    |3/4
+    |3/5
+    |0/1
+    |10/1
+    |9/10""".trimMargin().lines()
+
+    @Test
+    fun parsing() {
+        assertEquals(parseInput(rawInput),
+                mapOf(0 to listOf(0 to 2, 0 to 1),
+                        2 to listOf(0 to 2, 2 to 2, 2 to 3),
+                        1 to listOf(0 to 1, 10 to 1),
+                        3 to listOf(2 to 3, 3 to 4, 3 to 5),
+                        4 to listOf(3 to 4),
+                        5 to listOf(3 to 5),
+                        9 to listOf(9 to 10),
+                        10 to listOf(10 to 1, 9 to 10)))
+    }
+
+    @Test
+    fun generatePossibleConnections() {
+        val output = mutableSetOf<Set<Pair<Int, Int>>>()
+        getPossibleConnections(parseInput(rawInput), connections = output)
+        assertEquals(output.size, 12)
+        assertEquals(output, setOf(
+                setOf(),
+                setOf(0 to 2),
+                setOf(0 to 2, 2 to 2),
+                setOf(0 to 2, 2 to 2, 2 to 3),
+                setOf(0 to 2, 2 to 2, 2 to 3, 3 to 4),
+                setOf(0 to 2, 2 to 2, 2 to 3, 3 to 5),
+                setOf(0 to 2, 2 to 3),
+                setOf(0 to 2, 2 to 3, 3 to 4),
+                setOf(0 to 2, 2 to 3, 3 to 5),
+                setOf(0 to 1),
+                setOf(0 to 1, 10 to 1),
+                setOf(0 to 1, 10 to 1, 9 to 10)
+        ))
+        assertEquals(output.map { getConnectionValue(it) }.max(), 31)
+    }
+
+    @Test
+    fun bestBridge() {
+        val output = mutableSetOf<Set<Pair<Int, Int>>>()
+        getPossibleConnections(parseInput(rawInput), connections = output)
+        assertEquals(getBestBridge(output), 19)
+    }
+}
